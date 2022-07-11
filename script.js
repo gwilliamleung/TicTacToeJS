c275952cb03041ea7db8f5002c35035eff021320
William Leung <gwilliamleung@gmail.com>
2022-07-10 22:59:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let board = Array.from(document.getElementsByClassName('cell'));
+let turn = "X";
+
+function game(){
+    board.map( cells => {
+        cells.addEventListener('click', (e) => {
+            switch(turn){
+                case "X":
+                    if (e.target.innerHTML == ""){
+                        document.getElementById(e.target.id).innerHTML = "X";
+                        winCon(turn);
+                        turn = "O";
+                        break;
+                    }
+                case "O":
+                    if (e.target.innerHTML == ""){
+                        document.getElementById(e.target.id).innerHTML = "O";
+                        winCon(turn);
+                        turn = "X";
+                        break;
+                    }
+            }       
+            });
+        });
+}
+
+function reset(){
+    board.map( cells => {
+        cells.innerHTML = "";
+        });
+    turn = "X";
+}
+
+function winCon(){
+
+
+}
