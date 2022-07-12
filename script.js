ed90ec1597db10a35a3fe0297eb4f062a266a9ae
William Leung <gwilliamleung@gmail.com>
2022-07-11 23:01:19 -0400

    winArray and boardArr created

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let board = Array.from(document.getElementsByClassName('cell'));
 let turn = "X";
+let boardArr = [];
+const winArray = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
 
 function game(){
     board.map( cells => {
@@ -8,15 +19,25 @@ function game(){
                 case "X":
                     if (e.target.innerHTML == ""){
                         document.getElementById(e.target.id).innerHTML = "X";
+                        for (var i=0; i<board.length; i++){
+                            boardArr.push(board[i].innerHTML);
+                        }
                         winCon(turn);
                         turn = "O";
+                        console.log(boardArr)
+                        boardArr = [];
                         break;
                     }
                 case "O":
                     if (e.target.innerHTML == ""){
                         document.getElementById(e.target.id).innerHTML = "O";
+                        for (var i=0; i<board.length; i++){
+                            boardArr.push(board[i].innerHTML);
+                        }
                         winCon(turn);
                         turn = "X";
+                        console.log(boardArr)
+                        boardArr = [];
                         break;
                     }
             }       
@@ -31,5 +52,9 @@ function reset(){
     turn = "X";
 }
 
-function winCon(){
+
+
+function winCon(turn){
+
 }
+
